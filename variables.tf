a7abf9bebb708f1b56d7a43fc20c2e2979f51e3e
Joshua Hoblitt <josh@hoblitt.com>
2019-05-17 11:17:55 -0700

    add dns_overwrite var

--- variables.tf
+++ variables.tf
@@ -100,6 +100,11 @@ variable "dns_enable" {
   default     = false
 }
 
+variable "dns_overwrite" {
+  description = "overwrite pre-existing DNS records"
+  default     = false
+}
+
 locals {
   # remove "<env>-" prefix for production
   dns_prefix = "${replace("${var.env_name}-", "prod-", "")}"
