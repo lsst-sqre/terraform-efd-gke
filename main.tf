7913f21597a91f995aa1433b504facda9b5160ff
Joshua Hoblitt <josh@hoblitt.com>
2019-06-20 15:24:20 -0400

    pin efd mod to 7.7.7

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ provider "helm" {
 }
 
 module "efd" {
-  source = "git::https://github.com/lsst-sqre/terraform-efd.git//?ref=7be4e5276a318a15b7a514d3b00bde5739a58ace"
+  source = "git::https://github.com/lsst-sqre/terraform-efd.git//?ref=7.7.7"
 
   # replace with data lookup?
   domain_name = "${var.domain_name}"
